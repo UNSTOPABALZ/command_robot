d75a16eda29d3eff189060aa899bc3a51ec2e7ad
UNSTOPABALZ <unstopabalz@gmail.com>
2020-01-29 16:56:46 -0800

    message

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,64 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveSystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+
+/**
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final DriveSystem m_DriveSystem = new DriveSystem();
+
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_DriveSystem);
+
+
+
+  /**
+   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings.  Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+
+    Joystick ps4 = new Joystick(Constants.joystickPort);
+    double ps4_trigger = (ps4.getRawAxis(3) - ps4.getRawAxis(4)) * Constants.move_sensitivity;
+    double ps4_rotate = ps4.getX(Hand.kLeft) * Constants.rotate_sensitivity;
+  }
+
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_autoCommand;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class DriveSystem extends SubsystemBase {
+
+    public SpeedController frontLeft = new PWMVictorSPX(Constants.frontLeftMotor);
+    public SpeedController rearLeft = new PWMVictorSPX(Constants.rearLeftMotor);
+    public SpeedControllerGroup left = new SpeedControllerGroup(frontLeft, rearLeft);
+
+    public SpeedController frontRight = new PWMVictorSPX(Constants.frontRightMotor);
+    public SpeedController rearRight = new PWMVictorSPX(Constants.rearRightMotor);
+    public SpeedControllerGroup right = new SpeedControllerGroup(frontRight, rearRight);
+  
+    public DifferentialDrive robotDrive = new DifferentialDrive(left, right);
+
+  public DriveSystem() {
+    SpeedControllerGroup left = new SpeedControllerGroup(frontLeft, rearLeft);
+    SpeedControllerGroup right = new SpeedControllerGroup(frontRight, rearRight);
+    }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
