e5c27d88750a425d2a6784ccdabac5fcfe6f3502
UNSTOPABALZ <unstopabalz@gmail.com>
2020-02-05 16:19:12 -0800

    new code

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,30 +7,25 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.DriveSystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/**
- * An example command that uses an example subsystem.
- */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final DriveSystem m_subsystem;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(DriveSystem subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+public class DriveCommand extends CommandBase {
+  double m_time, m_lSpeed, m_rSpeed;
+
+  public DriveCommand(double time,double lSpeed, double rSpeed) {
+    m_time = time;
+    m_lSpeed = lSpeed;
+    m_rSpeed = rSpeed;
+    requires(Robot.DriveSystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    Robot.DriveSystem.setleft(m_lSpeed);
+    Robot.DriveSystem.setright(m_rSpeed);
+    setTimeout(m_time);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -41,11 +36,13 @@ public class ExampleCommand extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    Robot.DriveSystem.setleft(0);
+    Robot.DriveSystem.setright(0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return isTimedOut();
   }
 }
